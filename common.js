3f34b45a0c0184daf33b2621584a5a1bdf7fc675
Ryan Joseph <ryan@electricsheep.co>
2022-06-12 21:27:59 -0700

    start of 'serve' (plus 'putnew'); a bit of re-org

---
+++ common.js
@@ -0,0 +1,8 @@
+module.exports = {
+  constants: Object.freeze({
+    maximumContentLength: 2217,
+    protocolVersion: '83',
+    contentType: 'text/html;charset=utf-8',
+    authorizationPreamble: 'Spring-83 Signature'
+  })
+};
