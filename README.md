f59d210bbd4be07524978506314136fbff5bb751
Ryan Joseph <ryan@electricsheep.co>
2022-06-19 09:13:19 -0700

    add bulk posting to putnew; consume & produce canonical keypair file format; misc fixes

--- README.md
+++ README.md
@@ -14,28 +14,47 @@ $ npm install
 
 ### `findkey`
 
+**Update**: I'll maintain this script for good measure, but you should probably just use [PT's Go keyfinder](https://github.com/pteichman/ahoy) as it is undoubtedly <i>much</i> faster than this one.
+
 Randomly generates Ed25519 key pairs until one matching the specified format has been found.
 
 If called with `--strict`, will _only_ return when a key pair is found that is valid _this calendar year_.
 
-Output format is the following four lines:
-
-```
-privateKey <private key in hex>
-publicKey <public key in hex>
-<number of rounds required>
-<total runtime in milliseconds>
-```
+The output format matches the format that [Robin's client expects](https://github.com/robinsloan/spring-83-spec/tree/main/demo-client) and will be named in the same format.
 
 ### `putnew`
 
 ```
-$ putnew host privKeyHex htmlFile
+To put a single board: putnew host privKeyHex htmlFile
+To put path of boards: putnew boardPath hostsCommaSeperated
 ```
+
+Operates in two modes: single board PUT or multi-board PUT.
+
+#### Single-board
 
 Puts a new board with HTML source `htmlFile` to `host` with private key (in hex) `privKeyHex`.
 
-Will insert the required `<meta http-equiv="last-modified" content="...">` tag, with the appropriate `content` value, if it doesn't exist in the document already.
+Will insert the required `<time datetime="...">` tag, with the appropriate `datetime` value, if it doesn't exist in the document already.
+
+#### Multi-board
+
+Expects `boardPath` to be a directory containing board HTML files named with their full hex public key, and keypair files named according to the canonical pattern. So for example, a path named `./my-boards` with the following contents is valid (assuming the keypair files are valid of course):
+
+```
+1b30ed15e51e6faa3afb3605212d8cdb80177dfedc30a0d0939d26ec683e1023.html
+977b0a43de51225240128101105f0397cb2cde59801a74c40ad99c69183e0423.html
+spring-83-keypair-2022-06-19-1b30ed15e51e.txt
+spring-83-keypair-2022-06-19-977b0a43de51.txt
+```
+
+If `putnew` is then invoked like so:
+
+```
+$ putnew ./my-boards https://bogbody.biz,https://0l0.lol
+```
+
+it will post both boards to both listed hosts.
 
 ### `serve`
 
@@ -44,3 +63,7 @@ Runs a Spring-83 server on port specified by environment variable `SPRING83_BIND
 Writes boards into `SPRING83_CONTENT_DIR` (or `./.content` by default).
 
 If Docker is available, can be run simply with `docker compose up --build -d`: `SPRING83_CONTENT_DIR_HOST` must be specified to be the host-side path for which `SPRING83_CONTENT_DIR` will map to internally.
+
+Other environment variables you should set:
+  * `SPRING83_FQDN`: your fully-qualified domain name (no protocol scheme, e.g. `0l0.lol`)
+  * `SPRING83_CONTACT_ADDR`: a contact email address for your site
--- common.js
+++ common.js
@@ -1,3 +1,8 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
 const constants = Object.freeze({
   maximumContentLength: 2217,
   maximumNumberOfBoards: 10_000_000,
@@ -23,8 +28,33 @@ const constants = Object.freeze({
   defaultFQDN: 'example.com',
   strictVerification: true,
   maxKey64: (2 ** 64 - 1),
-  ttlCheckFreqMinutes: 11
+  ttlCheckFreqMinutes: 11,
+  keypairFilenamePrefix: 'spring-83-keypair'
 });
+
+function keyPairFilename (publicKey, root = __dirname) {
+  if (typeof publicKey !== 'string') {
+    return keyPairFilename(Buffer.from(publicKey).toString('hex'), root);
+  }
+
+  const sDate = new Date();
+  return path.join(root, [
+    constants.keypairFilenamePrefix,
+    sDate.toISOString().replace(/T.*$/, ''),
+    publicKey.slice(0, 12)
+  ].join('-') + '.txt');
+}
+
+async function findKeypairFile (publicKey, root) {
+  if (typeof publicKey !== 'string') {
+    return findKeypairFile(Buffer.from(publicKey).toString('hex'), root);
+  }
+  const pkPrefix = publicKey.slice(0, 12);
+  return path.join(root, await (await fs.promises.readdir(root)).find((p) => {
+    const parsed = path.parse(p);
+    return parsed.name.indexOf(constants.keypairFilenamePrefix) === 0 && parsed.name.indexOf(pkPrefix) !== -1;
+  }));
+}
 
 // 'strict' only allows keys that are usable *now* to match
 function pubKeyHexIsValid (pubKeyHex, strict = false) {
@@ -58,13 +88,46 @@ function getCurrentDifficultyFactor (knownKeys) {
   return (Object.keys(knownKeys).length / constants.maximumNumberOfBoards) ** 4;
 }
 
+async function readKeypairFile (filePath) {
+  const fStr = (await fs.promises.readFile(filePath)).toString('utf8');
+  return {
+    private: fStr.substring(0, 64),
+    public: fStr.substring(64, 128)
+  };
+}
+
+async function findKnownKeys (contentDir, expectMatchingKeyPairs = false) {
+  return (await Promise.all((await fs.promises.readdir(contentDir)).filter((p) => {
+    const parsed = path.parse(p);
+    return parsed.name.match(constants.keyMatchRegex) && parsed.ext === '.html';
+  })
+    .map(path.parse)
+    .filter(({ name }) => pubKeyHexIsValid(name, constants.strictVerification))
+    .map(async (pObj) => ([
+      pObj.name,
+      (await fs.promises.readFile(path.join(contentDir, pObj.name + pObj.ext))).toString('utf8'),
+      expectMatchingKeyPairs
+        ? await readKeypairFile(await findKeypairFile(pObj.name, contentDir))
+        : JSON.parse(await fs.promises.readFile(path.join(contentDir, pObj.name + '.json')))
+    ]))))
+    .reduce((a, [key, body, metadata]) => ({ [key]: { body, metadata }, ...a }), {});
+}
+
+function keyIsUnderDifficultyThreshold (pubKey, knownKeys) {
+  if (typeof pubKey === 'string') {
+    return keyIsUnderDifficultyThreshold(Buffer.from(pubKey, 'hex'), knownKeys);
+  }
+
+  return pubKey.readBigInt64BE() < BigInt(constants.maxKey64 * (1.0 - getCurrentDifficultyFactor(knownKeys)));
+}
+
 module.exports = {
   constants,
-
   pubKeyIsValid: (pubKeyData, strict = false) => pubKeyHexIsValid(Buffer.from(pubKeyData).toString('hex'), strict),
   pubKeyHexIsValid,
-
   getCurrentDifficultyFactor,
-  keyIsUnderDifficultyThreshold: (pubKeyHex, knownKeys) =>
-    Buffer.from(pubKeyHex, 'hex').readBigInt64BE() < BigInt(constants.maxKey64 * (1.0 - getCurrentDifficultyFactor(knownKeys)))
+  keyIsUnderDifficultyThreshold,
+  findKnownKeys,
+  keyPairFilename,
+  readKeypairFile
 };
