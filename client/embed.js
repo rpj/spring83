cfca7669e5b1a6f68744e4c93608fdafcbb3a64a
Ryan Joseph <ryan@electricsheep.co>
2022-06-24 16:45:16 -0700

    parse JSON out of comments for embedding

--- client/embed.js
+++ client/embed.js
@@ -1,16 +1,37 @@
-/* Embeds an 0l0.lol SpringBoard (by default, can be changed via the `springHost` parameter)
-   into a containing document.
+/* eslint-disable no-undef, no-unused-vars */
+'use strict';
+
+const defaultOptions = Object.freeze({
+  springHost: window.location.origin,
+  intContTagType: 'html',
+  embedMode: undefined
+});
+
+/* Utility functions that force the host to the one in the function name */
+const embed0l0Board = embedBoardForcedHost.bind(null, 'https://0l0.lol');
+const embedBogbodyBoard = embedBoardForcedHost.bind(null, 'https://bogbody.biz');
+const embedKindrobotBoard = embedBoardForcedHost.bind(null, 'https://spring83.kindrobot.ca');
+
+/* Embeds a SpringBoard into a containing document.
 
   @param `pubKey` the board to embed's public key (in hex)
   @param `contEleId` the outter container element to embed the board within
           (the element to which the shadow root will be attached)
-  @param `doneCb` a function to be called once the board has been embedded.
-          parameters passed will be the HTMLElement representing `contEleId`, and the update Date.
-  @param `intContTagType` the internal container element type to be created
-  @param `springHost` the host to fetch the board from
+  @param `options`:
+          `intContTagType`: the internal container element type to be created
+          `springHost`: the host to fetch the board from, defaults to window.location.origin
+          `embedMode`: if set to 'jsonOnly', will not embed the board's HTML into the outer document's
+            DOM, and will instead just call doneCb with any parsed JSON found.
+  @returns `Promise<object>` where object has properties:
+          * `containerElement`: the HTMLElement representing `contEleId`
+          * `upDate`: the last-modified Date
+          * `embeddedJson`: if any embedded JSON was found in comments and parsed successfully,
+            and array of those objects in the order they were found in the document
 */
-async function embedSpringBoard (pubkey, contEleId, doneCb, intContTagType = 'html', springHost = '0l0.lol') { // eslint-disable-line no-unused-vars
-  const res = await fetch(`https://${springHost}/${pubkey}`);
+async function embedSpringBoard (pubkey, contEleId, options = {}) {
+  const { springHost, intContTagType, embedMode } = { ...defaultOptions, ...options };
+
+  const res = await fetch(`${springHost}/${pubkey}`);
   if (res.ok) {
     const body = await res.text();
     const s83 = document.getElementById(contEleId);
@@ -24,24 +45,58 @@ async function embedSpringBoard (pubkey, contEleId, doneCb, intContTagType = 'ht
 
     try {
       const cont = document.createElement(intContTagType);
-      const parsedDoc = new DOMParser().parseFromString(body, 'text/html'); // eslint-disable-line no-undef
-      let targetNode = parsedDoc.firstChild;
+      const parsedDoc = new DOMParser().parseFromString(body, 'text/html');
 
-      while (!targetNode.firstChild || targetNode.nodeName === '#comment') {
-        targetNode = targetNode.nextSibling;
+      if (!parsedDoc) {
+        return;
       }
 
-      if (targetNode) {
-        cont.appendChild(targetNode);
-      } else {
-        cont.innerHTML = body;
+      const embeddedJson = parseCommentsForJson(parsedDoc);
+      let upDate;
+
+      if (embedMode !== 'jsonOnly') {
+        let targetNode = parsedDoc.firstChild;
+
+        // skips initial comment nodes
+        while (targetNode && (!targetNode.firstChild || targetNode.nodeName === '#comment')) {
+          targetNode = targetNode.nextSibling;
+        }
+
+        if (targetNode) {
+          cont.appendChild(targetNode);
+        } else {
+          cont.innerHTML = body;
+        }
+
+        s83.shadowRoot.appendChild(cont);
+        upDate = new Date(Date.parse(res.headers.get('last-modified')));
       }
 
-      s83.shadowRoot.appendChild(cont);
-      const upDate = new Date(Date.parse(res.headers.get('last-modified')));
-      doneCb?.(s83, upDate);
+      return {
+        containerElement: s83,
+        upDate,
+        embeddedJson
+      };
     } catch (err) {
       console.error(`Unparseable board: ${err}`);
     }
   }
+}
+
+async function embedBoardForcedHost (springHost, pubkey, contEleId, options = {}) {
+  return embedSpringBoard(pubkey, contEleId, { ...options, springHost });
+}
+
+function parseCommentsForJson (parentNode, retList = []) {
+  for (const child of parentNode.childNodes) {
+    if (child.nodeName === '#comment') {
+      try {
+        retList.push(JSON.parse(child.textContent));
+      } catch {}
+    } else if (child.childNodes.length > 0) {
+      retList = retList.concat(parseCommentsForJson(child, [...retList]));
+    }
+  }
+
+  return retList;
 }
